4aef23d003ae3899bba528521b4854792d8b5bb5
Sharansh24 <83573582+Sharansh24@users.noreply.github.com>
2022-04-25 22:55:35 +0530

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+    "name":"CombiCourse",
+    "short_name":"Combi",
+    "start_url":"index.html",
+    "display":"standalone",
+    "background_color":"#5900b3",
+    "theme_color":"black",
+    "scope": ".",
+    "description":"This is a PWA tutorial.",
+    "icons":[
+    {
+    "src":"website/img/Combicourse.png", 
+    "type":"image/png"
+    },
+    {
+    "src":"website/img/Combicourse.png",
+    "type":"image/png"
+    }
+]
+}
